371b4c0ada0ccfb1a56ad5703713522ab69bce98
unknown <shinchanmusic24@gmail.com>
2026-03-20 14:21:16 +0530

    db and schemas some changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
-app = FastAPI(title="Expense Tracker API", description="API for tracking expenses", version="1.0.0")
+# from app.core.config import settings
+# print(f"Database URL: {settings.DATABASE_URL}")
+from sqlalchemy import text
+
+
+
+app = FastAPI(
+    title="Expense Tracker API",
+      description="API for tracking expenses",
+        version="1.0.0")
 @app.get("/")
 def health_check():
     return{"status": "API is up and running!"}
+
+from app.db.database import Base, engine
+from app.models import expense
+from app.models import user
+
+Base.metadata.create_all(bind=engine)
